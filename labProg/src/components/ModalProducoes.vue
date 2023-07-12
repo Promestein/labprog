7f0b22a566e675ec8a9e192822929ebcf7202387
gabriel.costa <gabriel.costa@nca.ufma.br>
2023-07-12 13:52:13 -0300

    meu modal tá rosa

--- labProg/src/components/ModalProducoes.vue
+++ labProg/src/components/ModalProducoes.vue
@@ -1,5 +1,5 @@
 <template>
-    <q-dialog v-model="showModal" persistent>
+    <q-dialog class="dialog" v-model="showModal" persistent>
       <q-card class="modal">
         <q-card-section>
           <div class="title-page">{{ title }}</div>
@@ -61,6 +61,7 @@
   border-radius: 10px;
   border: 1px solid #ccc;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
+  background-color: #f3e8ff;
 }
 
 </style>
